a2e474107734dd7e3f2b91419ce82825f650c6ec
Sinan <sinanfen@hotmail.com>
2023-05-09 23:58:59 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  debugPrint('main metodu çalıştı');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
+    debugPrint('myapp metodu çalıştı');
     return MaterialApp(
       title: "My counter app",
-      theme: ThemeData(primarySwatch: Colors.teal),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: MyHomePage(),
     );
   }
@@ -19,9 +21,10 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
+ int _sayac = 0;
   @override
   Widget build(BuildContext context) {
+    debugPrint('myhomepage metodu çalıştı');
     return Scaffold(
       appBar: AppBar(
         title: Text('My Counter Zort'),
@@ -35,13 +38,20 @@ class MyHomePage extends StatelessWidget {
             style: TextStyle(fontSize: 24),
           ),
           Text(
+            _sayac.toString(),
             '0',
             style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
           ),
         ],
       )),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint('buton metodu çalıştı');
+            sayaciArttır();
+          },
+          child: Icon(Icons.add)),
     );
   }
+  
+  void sayaciArttır() {}
 }
