04e350e1b3ed9f7f491466e63bc64b7aeed51a61
Sinan <sinanfen@hotmail.com>
2023-05-09 05:02:32 +0300

    stateles widged

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "My counter app",
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Counter Zort'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Butona basılma sayısı',
+            style: TextStyle(fontSize: 24),
+          ),
+          Text(
+            '0',
+            style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+          ),
+        ],
+      )),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+    );
   }
 }
