bb6b010985e40bfe4efa84136416853a973d1ef4
Sinan <sinanfen@hotmail.com>
2023-05-11 23:23:07 +0300

    updated :106

--- lib/image_widgets.dart
+++ lib/image_widgets.dart
@@ -5,21 +5,53 @@ class ImageOrnekleri extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String _kennyImgURL =
+        "https://static.wikia.nocookie.net/southpark/images/6/6f/KennyMcCormick.png/revision/latest?cb=20160409020502";
+    String _stanImgURL = "https://pngimg.com/d/south_park_PNG30.png";
+    String _wendyImgURL =
+        "https://assets.stickpng.com/images/587170697b7f6103e35c6cb1.png";
     return Center(
-      child: Column(
-        children: [
-          Container(
-              color: Colors.purple.shade100,
-              child: Image.asset('assets/images/Screenshot_1.png')),
-          Container(
-            width: 75,
-            height: 75,
-            child: CircleAvatar(
-              child: Text('Deneme'),
+        child: Column(children: [
+      IntrinsicHeight(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                  child: Image.network(
+                _stanImgURL,
+                fit: BoxFit.cover,
+              )),
             ),
-          )
-        ],
+            Expanded(
+                child: Container(
+              child: Image.network(
+                _kennyImgURL,
+                fit: BoxFit.cover,
+              ),
+            )),
+            Expanded(
+              child: Container(
+                  child: Image.network(
+                _wendyImgURL,
+                fit: BoxFit.cover,
+              )),
+            )
+          ],
+        ),
       ),
-    );
+      Expanded(
+        child: Container(
+          width: 200,
+          child: FadeInImage.assetNetwork(
+              placeholder: 'assets/images/loading.gif', image: _stanImgURL),
+        ),
+      ),
+      Expanded(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Placeholder(),
+      )),
+    ]));
   }
 }
